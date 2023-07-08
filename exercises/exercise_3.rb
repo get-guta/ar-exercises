8a495886c5f1bfd635cca7c80e8c3a8907a92dc0
getenesh <getenesh.guta@gmail.com>
2023-07-08 11:52:57 -0400

    exersice 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+@store3 = Store.find(3)
+
+@store3.destroy
+
+store_count = Store.count
+puts "Number of stores after deletion: #{store_count}"
+
